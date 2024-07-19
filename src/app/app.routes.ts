d9d5244fc984da168a004e9f14f60f8ed5bf4378
Rungatang <rahulsrivastava4feb@gmail.com>
2024-07-19 15:30:04 +0530

    initial commit 1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,7 +7,10 @@ export const routes: Routes = [
   { path: '', redirectTo: '/user-list', pathMatch: 'full' },
   { path: 'user-list', component: UserListComponent },
   { path: 'user-form', component: UserFormComponent },
-  { path: 'user-details/:name', component: UserDetailsComponent }
+  { path: 'user-details/:name', component: UserDetailsComponent },
+  {path : '', redirectTo : 'home', pathMatch: 'full'},
+  {path: '**', redirectTo: 'home', pathMatch:'full'}
+
   ];
   
 
